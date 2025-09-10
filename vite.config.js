dae5fd363bcc6798acf588191e2ca35f6ef65cf0
MaryamAli10 <maryamalibaksh10@gmail.com>
2025-09-10 17:48:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://maryamali10.github.io/husna-gardens-project/",
 
   resolve: {
     alias: {
